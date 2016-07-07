41dfc83307e981c4e53d0d3ce191f79b59d5d580
Anthony Albertorio <anthony.albertorio@gmail.com>
2016-07-07 01:20:18 -0400

    change click function to work with new sumbit button

--- js/weather.js
+++ js/weather.js
@@ -1,31 +1,106 @@
+/*
+features to add:
+- add rest of info needed
 
-// ran when submitted
-$('form').submit(function(evt){
+- style up to look better
+  -fix weather dock to look better
+  -better font
+  -background style cartoony to match weather
+    - pick color templates
+
+  - how to add map to weather map
+
+- error handler for city not found
+*/
+
+// global variables
+var $citySearchField = $('#city-name');
+var citySearch = $('#city-name').val();
+var $submitButton = $('#submit');
+var unit = "metric";
+
+// weather dock variables
+var $temperture = $('#temperture');
+var $humidity = $('#humidity');
+var $sky = $('#sky');
+var $wind = $('#wind');
+
+// location and weather
+var currentLocation = findLocation();
+weatherQuery(currentLocation);
+
+// one method of finding current location based on IP
+function findLocation(){
+  // location address API
+  // corsorigin.me to get around CORS issues in codepen
+  var getLocationAPI = 'https://crossorigin.me/http://ipinfo.io';
+  // success callback
+  function locationSuccess(data){
+    var location = data.city.toString() + ", " + data.country.toString();
+    // add location to field
+    $citySearchField.val(location);
+  }
+  // actual JSON call
+  $.getJSON(getLocationAPI,locationSuccess);
+} // end findLocation
+
+
+// Unit: either metric or imperial
+$("input[name=standard]:radio").click(function(){
+  var $imperial = $("#imperial");
+  if($imperial.is(":checked")) {
+    unit = "imperial";
+  } else {
+    unit = "metric";
+  }
+}); //end metric/imperial selection 
+
+
+function weatherQuery(city){
+  var openWeatherAPI = 'https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?';
+  var cityLookup = "q=" + city;
+  var unitType = "&units=" + unit;
+  var apiKey = "&APPID=4c3a82148c102d600c31feb9ab9a3b94";
+  var openWeatherPath = openWeatherAPI+ cityLookup + unitType + apiKey;
+
+  // API callback
+  function openWeatherAPISuccess(data){
+    // when successful, search button allowed again
+    $citySearchField.prop("disabled", false);
+    $submitButton.attr("disabled", false).val('Go');
+
+    var temp = data.main.temp;
+    var humidity = data.main.humidity + "%";
+
+    if(unit === "metric"){
+      temp += " C";
+    } else {
+      temp += " F";
+    }
+
+    // weather dock
+    $temperture.html('<li> Temp: ' +  temp +'</li>');
+    $humidity.html('<li> humidity: ' + humidity +'</li>');
+    $sky.html('<li>30</li>');
+    $wind.html('<li>40</li>');
+  }
+    
+  // API callback
+  $.getJSON(openWeatherPath, openWeatherAPISuccess);
+} // end weatherQuery
+
+
+// Run when go clicked and new location submitted
+$($submitButton).click(function(evt){
   // prevent form from going to a new site.
   evt.preventDefault();
-  
 
-  var citySearch = $('#search').val();
-  var $citySearchField = $('#search');
-
-  var $submitButton = $('#submit');
+  // disable search until we get data
   $citySearchField.prop("disabled", true);
   $submitButton.attr("disabled", true).val('searching...');
 
-  // the AJAX part
-  var openWeatherMapAPI = "api.openweathermap.org/data/2.5/forecast/city?id=" + citySearch + "&APPID=4c3a82148c102d600c31feb9ab9a3b94";
-  
-  // parameters to pass
-  var openWeatherMapOptions = {
-    // q is for the city
-    q: citySearch,
-    format: "json"
-  };
-
-  // callback function 
-  function displayWeather(data) {
-    alert(data);
-  }
-
-  $.getJSON(openWeatherMapAPI, openWeatherMapOptions, displayWeather);
+  //query the weather
+  citySearch = $('#city-name').val();
+  weatherQuery(citySearch);
 }); // end submit
+
